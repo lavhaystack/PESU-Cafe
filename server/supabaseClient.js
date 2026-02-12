645cf0a075de6789b924c6ebb3755459f4513b11
saessei <danielleanne.poral-22@cpu.edu.ph>
2026-02-12 16:28:36 +0800

    add supabaseClient

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
+dotenv.config()
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
+
+export default supabase
